15378317098b5d7d3c916e9a7ddf4ce636dad0c3
DelphyM <dmladek@netbeans.org>
2023-09-29 12:21:03 +1300

    demo - m05-ex03 - Entity Inheritance - Single Table per Class Hierarchy
    Similar to m05-ex01
    
    * POM and persistence xml files
    * example code

---
+++ 05/demos/Before (1)/Before/m05-ex03/src/main/java/com/pluralsight/hibernatefundamentals/airport/OneWayTicket.java
@@ -0,0 +1,21 @@
+package com.pluralsight.hibernatefundamentals.airport;
+
+import java.time.LocalDate;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("O")
+public class OneWayTicket extends Ticket {
+
+    private LocalDate latestDepartureDate;
+
+    public LocalDate getLatestDepartureDate() {
+        return latestDepartureDate;
+    }
+
+    public void setLatestDepartureDate(LocalDate latestDepartureDate) {
+        this.latestDepartureDate = latestDepartureDate;
+    }
+}
---
+++ 05/demos/Before (1)/Before/m05-ex03/src/main/java/com/pluralsight/hibernatefundamentals/airport/Ticket.java
@@ -0,0 +1,34 @@
+package com.pluralsight.hibernatefundamentals.airport;
+
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TICKETS")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TICKET_TYPE")
+public abstract class Ticket {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String number;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
