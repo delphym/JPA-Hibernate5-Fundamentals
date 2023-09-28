d66056ed1fb76407226d5b3bb6bf3bd4d5854a1b
DelphyM <dmladek@netbeans.org>
2023-09-29 11:35:46 +1300

    demo - m05-ex01 - Entity Inheritance - Extend One Entity
    * POM and persistence xml files
    * example code

---
+++ 05/demos/Before (1)/Before/m05-ex01/src/main/java/com/pluralsight/hibernatefundamentals/airport/Ticket.java
@@ -0,0 +1,35 @@
+package com.pluralsight.hibernatefundamentals.airport;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TICKETS")
+public abstract class Ticket {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String number;
+
+    public Ticket(String number) {
+        this.number = number;
+    }
+
+    public Ticket() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
